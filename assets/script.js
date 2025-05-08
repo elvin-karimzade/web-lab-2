7b6ed54711f2ba9723c0e43c9f60ffd03c4473bc
elvin-karimzade <elvin7799822@gmail.com>
2025-05-08 23:15:50 +0400

    Lab committed

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+// script.js
+function downloadFile(url, message) {
+    alert(message);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = '';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
